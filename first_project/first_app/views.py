cf79ba1c5d3b70fd8f90b69025e387920335bfb9
Nakul N <nakulimpo@gmail.com>
2026-04-20 23:16:52 +0530

    learning_part1

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the first_app index.")
+    my_dict = {'insert_me':"hey so im coming from first_app\index.html file so ya lesgooo"}
+    return render(request, 'first_app\index.html', context=my_dict)
